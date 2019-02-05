5f4735f23a4b05b417496e147e77b932d5d662aa
m.biryukov <mibir85@gmail.com>
2019-02-05 18:27:43 +0300

    Initial commit for 2nd client

--- src/main/java/test/Engine.java
+++ src/main/java/test/Engine.java
@@ -12,7 +12,7 @@ import javax.annotation.PostConstruct;
 
 import static org.apache.ignite.events.EventType.EVT_CACHE_OBJECT_PUT;
 import static org.apache.ignite.events.EventType.EVT_CACHE_OBJECT_REMOVED;
-import static org.apache.ignite.transactions.TransactionConcurrency.OPTIMISTIC;
+import static org.apache.ignite.transactions.TransactionConcurrency.PESSIMISTIC;
 
 @Component
 public class Engine {
@@ -47,7 +47,7 @@ public class Engine {
     }
 
     public void addOrReplaceCollectionValue(Integer cacheKey, Integer key, String value) throws PutCacheException {
-        try (Transaction tx = ignite.transactions().txStart(OPTIMISTIC, TransactionIsolation.REPEATABLE_READ)) {
+        try (Transaction tx = ignite.transactions().txStart(PESSIMISTIC, TransactionIsolation.SERIALIZABLE)) {
             Data data = get(cacheKey);
             data.addOrReplaceValue(key, value);
             Data data1 = get(cacheKey);
@@ -62,22 +62,22 @@ public class Engine {
 
     public void setLocalListenerAnyKey(CacheUpdateListener cacheUpdateListener) {
         IgnitePredicate<CacheEvent> localListener = createListenerAnyKey(cacheUpdateListener);
-        ignite.events(ignite.cluster().forCacheNodes("smartws")).localListen(localListener, EVT_CACHE_OBJECT_PUT, EVT_CACHE_OBJECT_REMOVED);
+        ignite.events(ignite.cluster().forCacheNodes("CACHE_NAME")).localListen(localListener, EVT_CACHE_OBJECT_PUT, EVT_CACHE_OBJECT_REMOVED);
     }
 
     public void setRemoteListenerAnyKey(CacheUpdateListener cacheUpdateListener) {
         IgnitePredicate<CacheEvent> remoteListener = createListenerAnyKey(cacheUpdateListener);
-        ignite.events(ignite.cluster().forCacheNodes("smartws")).remoteListenAsync(null, remoteListener, EVT_CACHE_OBJECT_PUT, EVT_CACHE_OBJECT_REMOVED);
+        ignite.events(ignite.cluster().forCacheNodes("CACHE_NAME")).remoteListenAsync(null, remoteListener, EVT_CACHE_OBJECT_PUT, EVT_CACHE_OBJECT_REMOVED);
     }
 
     public void setLocalListenerCurrentKey(CacheUpdateListener cacheUpdateListener, Integer key) {
         IgnitePredicate<CacheEvent> localListener = createListenerCurrentKey(key, cacheUpdateListener);
-        ignite.events(ignite.cluster().forCacheNodes("smartws")).localListen(localListener, EVT_CACHE_OBJECT_PUT, EVT_CACHE_OBJECT_REMOVED);
+        ignite.events(ignite.cluster().forCacheNodes("CACHE_NAME")).localListen(localListener, EVT_CACHE_OBJECT_PUT, EVT_CACHE_OBJECT_REMOVED);
     }
 
     public void setRemoteListenerCurrentKey(CacheUpdateListener cacheUpdateListener, Integer key) {
         IgnitePredicate<CacheEvent> remoteListener = createListenerCurrentKey(key, cacheUpdateListener);
-        ignite.events(ignite.cluster().forCacheNodes("smartws")).remoteListenAsync(null, remoteListener, EVT_CACHE_OBJECT_PUT, EVT_CACHE_OBJECT_REMOVED);
+        ignite.events(ignite.cluster().forCacheNodes("CACHE_NAME")).remoteListenAsync(null, remoteListener, EVT_CACHE_OBJECT_PUT, EVT_CACHE_OBJECT_REMOVED);
     }
 
     private IgnitePredicate<CacheEvent> createListenerAnyKey(final CacheUpdateListener cacheUpdateListener) {
